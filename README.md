862e9dd1538128c4023b87473806417ddd0c4086
yrac <yrac@developer>
2017-09-22 07:47:00 -0400

    !avances

--- README.md
+++ README.md
@@ -3,11 +3,16 @@ SPA Proxy FrondEnd
 
 This module add proxy for SPA PROJECT based in MAVEN Directory
 
-## Required:
+## Use:
 
-1. SPA: Maven SPA Project with the main script
-2. STATIC: Maven STATIC Project with global resources
-3. SHOWCASE: Maven SHOWCASE Project with examples
+1. Project Maven: Any Project with same structure:
+```
+ project-pom/
+   project-impl/
+   project-web/  //Web Project
+   gulpfile.js
+```
+
 
 ## Used:
 
--- spa-proxy.js
+++ spa-proxy.js
@@ -1,127 +1,94 @@
 var gutil = require('gulp-util');
 var proxy = require("http-proxy-middleware");
 
-function SpaProxy(app, mod, path) {
-	context = path || mod;
-	project = app + '-' + mod + '-web';
-	var dir = ['part', 'view', 'ctrl', 'css', 'js'];
-	var pathSpa, pathStatic, pathShowcase;
-
-	this.withStatic = function (path, static) {
-		if (path) {
-			static = static || 'static-web';
-			pathStatic = path + '/' + static + '/src/main/webapp';
-		} else {
-			pathStatic = undefined;
-		}
-	};
-
-	this.withShowcase = function (path, showcase) {
-		if (path) {
-			showcase = showcase || 'showcase-web';
-			pathShowcase = path + '/' + showcase + '/src/main/webapp';
-		} else {
-			pathShowcase = undefined;
-		}
-	};
-
-	this.withSpa = function (path, main) {
-		if (path) {
-			main = main || 'spa-web';
-			pathSpa = path + '/' + main + '/src/main/webapp';
-		} else {
-			pathSpa = undefined;
-		}
-	};
-
-	this.createRoutes = function () {
-		var routes = {};
-		for (var i in dir) {
-			var dirName = dir[i];
-			var key = '/' + context + '/' + dirName + '/';
-			var value = './' + project + '/src/main/webapp/' + dirName + '/';
-			routes[key] = value;
-		}
-		if (pathStatic) {
-			routes['/static'] = pathStatic;
-		}
-		if (pathShowcase) {
-			routes['/static/showcase'] = pathShowcase;
-		}
-		if (pathSpa) {
-			routes['/index.html'] = pathSpa + '/index.html';
-			routes['/broken'] = pathSpa + '/broken';
-			routes['/jsx'] = pathSpa + '/jsx';
-		}
-		return routes;
-	};
-
-	this.createRules = function () {
-		var rules = [];
-		var ix = 0;
-		rules[ix++] = '!/browser-sync/**';
-		for (var i in dir) {
-			var dirName = dir[i];
-			rules[ix++] = '!/' + context + '/' + dirName + '/**';
-		}
-		if (pathStatic) {
-			rules[ix++] = '!/static/**';
-		}
-		if (pathShowcase) {
-			rules[ix++] = '!/static/showcase/**';
-		}
-		if (pathSpa) {
-			rules[ix++] = '!/index.html';
-			rules[ix++] = '!/broken/**';
-			rules[ix++] = '!/jsx/**';
-		}
-		rules[ix++] = '**';
-		return rules;
-	};
-
-	this.createWatchPath = function () {
-		var path = [];
-		var ix = 0;
-		for (var i in dir) {
-			var dirName = dir[i];
-			path[ix++] = './' + project + '/src/main/webapp/' + dirName + '/**/*.*';
-		}
-		if (pathStatic) {
-			path[ix++] = pathStatic + '/**/*.*';
-		}
-		if (pathShowcase) {
-			path[ix++] = pathShowcase + '/**/*.*';
-		}
-		if (pathSpa) {
-			path[ix++] = pathSpa + '/index.html';
-			path[ix++] = pathSpa + '/broken/**/*.*';
-			path[ix++] = pathSpa + '/jsx/**/*.*';
-		}
-		return path;
-	};
-
-	this.createServer = function (targetServer) {
-		var _routes = this.createRoutes();
-		var _rules = this.createRules();
-		gutil.log('Routes:', _routes);
-		var server = {
-			baseDir: [],
-			routes: _routes
+function SpaProxy(project, context) {
+		var noProxyDirs = ['part', 'view', 'ctrl', 'css', 'js'];
+		var mavenProjectArray = [{project: project, context: context || project, path: '.'}];
+		var util = {
+				assertURL: function(url){
+						url = url.replace('//','/');
+						url = url.replace('//','/');
+						return url;
+				}
 		};
-		server.middleware = proxy(_rules, {
-			target: targetServer,
-			changeOrigin: true,
-			ws: true,
-			//logLevel:'info'
-			onProxyRes: function (proxyRes, req, res) {
-				gutil.log('Proxy for:', req.url);
-			}
-		});
-		return server;
-	};
+		this.noProxy = {
+				add: function (dir) {
+						noProxyDirs.push(dir);
+				},
+				set: function (dirs = []) {
+						noProxyDirs = dirs;
+				}
+		};
+
+		this.mavenProject = {
+				add: function (project, context, path) {
+						mavenProjectArray.push({project: project, context: context, path: path});
+				},
+				set: function (array = []) {
+						mavenProjectArray = array;
+				}
+		};
+
+		this.createRoutes = function () {
+				var routes = {};
+				mavenProjectArray.forEach(function (maven) {
+						noProxyDirs.forEach(function (directory) {
+								var key = '/' + maven.context + '/' + directory + '/';
+								key = util.assertURL(key);
+								var value = maven.path + '/' + maven.project + '/src/main/webapp/' + directory + '/';
+								routes[key] = value;
+						});
+				});
+				return routes;
+		};
+
+		this.createRules = function () {
+				var rules = [];
+				rules.push('!/browser-sync/**');
+				mavenProjectArray.forEach(function (maven) {
+						noProxyDirs.forEach(function (directory) {
+								var contextPath = '!/' + maven.context + '/' + directory + '/**';
+								contextPath = util.assertURL(contextPath);
+								rules.push(contextPath);
+						});
+				});
+				rules.push('**');
+				return rules;
+		};
+
+		this.createWatchPath = function () {
+				var watchDir = [];
+				mavenProjectArray.forEach(function (maven) {
+						noProxyDirs.forEach(function (directory) {
+								var directoryPath = maven.path + '/' + maven.project + '/src/main/webapp/' + directory + '/**/*.*';
+								directoryPath = util.assertURL(directoryPath);
+								watchDir.push(directoryPath);
+						});
+				});
+				return watchDir;
+		};
+
+		this.createServer = function (targetServer) {
+				var _routes = this.createRoutes();
+				var _rules = this.createRules();
+				gutil.log('Routes:', _routes);
+				var server = {
+						baseDir: [],
+						routes: _routes
+				};
+				server.middleware = proxy(_rules, {
+						target: targetServer,
+						changeOrigin: true,
+						ws: true,
+						onProxyRes: function (proxyRes, req, res) {
+								gutil.log('Proxy for:', req.url);
+						}
+				});
+				return server;
+		};
 }
 
-module.exports = function (app, mod, path) {
-	gutil.log('Create Config for: ' + app + '-' + mod + ' = ' + path);
-	return new SpaProxy(app, mod, path);
+module.exports = function (project, context) {
+		gutil.log('SPA Proxy for: ' + project + ' url:' + context);
+		return new SpaProxy(project, context);
 };
